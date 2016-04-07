21462bfacee2846494b99561f676848d3395f524
deejanzen <djanzen@email.arizona.edu>
2016-04-06 18:17:24 -0700

    setting the file up

--- src/RedBlackNode.java
+++ src/RedBlackNode.java
@@ -7,11 +7,22 @@ public class RedBlackNode {
     private String key;
     private RedBlackNode left;
     private RedBlackNode right;
+    private boolean black;
     private String internalName;
-    private boolean color;
 
-    public static RedBlackNode buildFrom234(Node234 root ){
+    public RedBlackNode(String key, RedBlackNode left, RedBlackNode right, boolean black){
+        this.key = key;
+        this.left = left;
+        this.right = right;
+        this.black = black;
+        internalName = "Vertex_" + hashCode();
+    }
 
+    public static RedBlackNode buildFrom234(Node234 root){
+        //Your program must turn all 2-3-4 nodes with 2 key
+        //in to a left-leaning widget (that is, with the red node on the left)
+        //However, you will enforce this only in this method; do not enforce it on insertions!
+        return new RedBlackNode(null, null, null, false);
     }
 
     public String[] toArray(){
@@ -19,21 +30,21 @@ public class RedBlackNode {
         return result;
     }
 
-    public String[] toArray_PreOrder(){
+    public String[] toArray_preOrder(){
         String [] result = {};
         return result;
     }
 
-    public String[] insert_td(String key){
-
+    public RedBlackNode insert_td(String key){
+        return new RedBlackNode(null, null, null, false);
     }
 
-    public String[] insert_bu(String key){
-
+    public RedBlackNode insert_bu(String key){
+        return new RedBlackNode(null, null, null, false);
     }
 
     //toDotFile is generating a digraph. Boolean parameter per spec but unused. Using two helpers for vertices and edges
-    public String toDotFile(Boolean b){
+    public String toDotFile(Boolean isRoot){
         String result = "digraph {\n" + "\t" + getInternalName() + "[penwidth=3];\n" + toDotFileHelperLabel(new String(""));
         result += toDotFileHelperEdge(new String(""));
         return result + "}\n";
@@ -69,9 +80,41 @@ public class RedBlackNode {
 
     }
 
-    //using hashCode per spec suggestion
-    private void setInternalName() {
-        internalName = "Vertex_" + hashCode();
+    public String getKey(){
+        return key;
     }
 
-}
+    public void setKey(String key){
+        this.key = key;
+    }
+
+    public RedBlackNode getLeft(){
+        return left;
+    }
+
+    public void setLeft(RedBlackNode left){
+        this.left = left;
+    }
+
+    public RedBlackNode getRight(){
+        return right;
+    }
+
+    public void setRight(RedBlackNode right){
+        this.right = right;
+    }
+
+    //using hashCode per spec suggestion
+    public String getInternalName(){
+        return internalName;
+    }
+
+    public boolean getBlack(){
+        return black;
+    }
+
+    public void setBlack(boolean black){
+        this.black = black;
+    }
+
+}//end RedBlackNode
